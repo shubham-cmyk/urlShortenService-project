d9d8fa18fed74f51adb07902307c1a40e199115f
shubham <iamshubhamgupta2001@gmail.com>
2022-09-16 22:27:40 +0530

    change json

--- models/models.go
+++ models/models.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type Request struct {
-	URL_LONG   string        `json:"url_long"`
-	URL_SHORT  string        `json:"url_short"`
+	URL_LONG   string        `json:"url"`
+	URL_SHORT  string        `json:"short"`
 	Expiration time.Duration `json:"expiration"`
 }
 
 type Response struct {
-	URL_LONG  string `json:"url_long"`
-	URL_SHORT string `json:"url_short"`
+	URL_LONG  string `json:"url"`
+	URL_SHORT string `json:"short"`
 }
