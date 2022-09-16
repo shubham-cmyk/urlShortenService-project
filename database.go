8bdab770c7779894e2888608c118857d289edf2a
shubham <iamshubhamgupta2001@gmail.com>
2022-09-16 16:17:51 +0530

    adding_database_client

---
+++ database.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var Ctx = context.Background()
+
+func CreateClient(dint int) *redis.Client {
+
+	redisDatabase := redis.NewClient(&redis.Options{
+		DB:       dint,
+		Addr:     "db:6379",
+		Password: "",
+	})
+
+	return redisDatabase
+
+}
